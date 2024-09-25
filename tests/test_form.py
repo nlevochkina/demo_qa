b710246b7c1f1db4f2c78add0481158d8f485f2a
nlevochkina <nlevochkina@ozon.ru>
2024-09-26 00:01:10 +0300

    Автотест на заполнение формы

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,4 +1,3 @@
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selene import browser, be
 
@@ -11,11 +10,11 @@ def test_fill_out_the_form():
     
     """
 
-    browser.element('#firstName').should(be.blank).type('Наталия').press_enter()
-    browser.element('#lastName').should(be.blank).type('Левочкина').press_enter()
-    browser.element('#userEmail').should(be.blank).type('natalia@mail.ru').press_enter()
+    browser.element('#firstName').should(be.blank).type('Наталия')
+    browser.element('#lastName').should(be.blank).type('Левочкина')
+    browser.element('#userEmail').should(be.blank).type('natalia@mail.ru')
     browser.element('label[for="gender-radio-2"]').click()
-    browser.element('#userNumber').should(be.blank).type('790012345678').press_enter()
+    browser.element('#userNumber').should(be.blank).type('790012345678')
 
     """
     Заполняем опциональные поля формы
@@ -23,14 +22,16 @@ def test_fill_out_the_form():
     """
 
     browser.element('#dateOfBirthInput').click()
-    browser.driver.find_element(By.CLASS_NAME, 'react-datepicker__month-select')
-    browser.driver.find_element(By.XPATH, "//option[@value='0']").click()
-    browser.driver.find_element(By.CLASS_NAME, 'react-datepicker__year-dropdown-container--select')
-    browser.driver.find_element(By.XPATH, "//option[@value='1990']").click()
-    browser.driver.find_element(By.CLASS_NAME, 'react-datepicker__month')
+    browser.driver.find_element(By.XPATH, "//*[@class='react-datepicker__month-select']//option[@value='0']").click()
+    browser.driver.find_element(By.XPATH,
+                                "//*[@class='react-datepicker__year-select']//option[@value='1990']").click()
     browser.driver.find_element(By.CLASS_NAME, 'react-datepicker__day--012').click()
-
-    # browser.driver.find_element(By.CSS_SELECTOR, 'subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3').send_keys('b' + Keys.ENTER)
     browser.element('label[for="hobbies-checkbox-1"]').click()
-    browser.element('#currentAddress').should(be.blank).type('Москва, улица Московская, дом 56').press_enter()
+    browser.element('#currentAddress').should(be.blank).type('Москва, улица Московская, дом 56')
+    browser.element('#uploadPicture').send_keys('/Users/nlevochkina/PycharmProjects/demo_qa/images/sun.jpeg')
 
+    """
+    Отправляем заполненную форму
+    
+    """
+    browser.element('#submit').click()

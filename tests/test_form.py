d3f473c4ab24dd76af57d68fd017eba545e16262
nlevochkina <nlevochkina@ozon.ru>
2024-09-27 10:50:12 +0300

    Автотест на заполнение формы

--- tests/test_form.py
+++ tests/test_form.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selene import browser, be, have
 
 
-def test_fill_out_the_form(open_browser_demo_qa):
+def test_fill_out_the_form():
     browser.open('https://demoqa.com/automation-practice-form')
 
     browser.driver.execute_script("$('#fixedban').remove()")
@@ -13,7 +13,6 @@ def test_fill_out_the_form(open_browser_demo_qa):
     # WHEN
 
     browser.element('#firstName').should(be.blank).type('Natalia')
-    assert browser.element('#firstName').get_attribute('value') == 'Natalia'
     browser.element('#lastName').should(be.blank).type('La')
     browser.element('#userEmail').should(be.blank).type('natalia@mail.ru')
     browser.element('label[for="gender-radio-2"]').click()
@@ -30,7 +29,7 @@ def test_fill_out_the_form(open_browser_demo_qa):
     browser.element('label[for="hobbies-checkbox-1"]').click()
     browser.element('#currentAddress').should(be.blank).type('Moscow Street, 56, Moscow, Russia')
 
-    image_path = os.path.abspath('./images/sun.jpeg')
+    image_path = os.path.abspath('../demo_qa/images/sun.jpeg')
     browser.element('#uploadPicture').send_keys(image_path)
 
     browser.element('#state').click()
@@ -41,17 +40,14 @@ def test_fill_out_the_form(open_browser_demo_qa):
     browser.element('#submit').press_enter()
 
     # THEN
-    browser.element('.table').all('td').even.should(
-        have.exact_texts(
-            'Natalia La',
-            'natalia@mail.ru',
-            'Female',
-            '1234567890',
-            '12 January,1990',
-            'Computer Science',
-            'Sports',
-            'sun.jpeg',
-            'Moscow Street, 56, Moscow, Russia',
-            'Haryana Karnal'
-        )
-    )
+    browser.element('//td[contains(text(),"Student Name")]/../td[2]').should(have.exact_text('Natalia La'))
+    browser.element('//td[contains(text(),"Student Email")]/../td[2]').should(have.exact_text('natalia@mail.ru'))
+    browser.element('//td[contains(text(),"Gender")]/../td[2]').should(have.exact_text('Female'))
+    browser.element('//td[contains(text(),"Mobile")]/../td[2]').should(have.exact_text('1234567890'))
+    browser.element('//td[contains(text(),"Date of Birth")]/../td[2]').should(have.exact_text('12 January,1990'))
+    browser.element('//td[contains(text(),"Subject")]/../td[2]').should(have.exact_text('Computer Science'))
+    browser.element('//td[contains(text(),"Hobbies")]/../td[2]').should(have.exact_text('Sports'))
+    browser.element('//td[contains(text(),"Picture")]/../td[2]').should(have.exact_text('sun.jpeg'))
+    browser.element('//td[contains(text(),"Address")]/../td[2]').should(
+        have.exact_text('Moscow Street, 56, Moscow, Russia'))
+    browser.element('//td[contains(text(),"State and City")]/../td[2]').should(have.exact_text('Haryana Karnal'))

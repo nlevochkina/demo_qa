587f5848017464af7164b1d949cf2de982720044
nlevochkina <nlevochkina@ozon.ru>
2024-10-03 19:49:57 +0300

    Работа с файлами

---
+++ tests/test_create.py
@@ -0,0 +1,41 @@
+import os
+import zipfile
+import pandas as pd
+
+from pypdf import PdfReader
+
+path = os.path.dirname(os.path.abspath(__file__))
+print(path)
+path_to_tmp = os.path.abspath(os.path.join(path, '..', 'tmp'))
+path_to_archive = os.path.abspath(os.path.join(path_to_tmp, 'files_zip.zip'))
+
+
+def test_add_files_to_zip():
+    with zipfile.ZipFile(path_to_archive, 'w') as zf:
+        files_to_add = ['pdf_file.pdf', 'xlsx_file.xlsx', 'csv_file.csv']
+        for file in files_to_add:
+            add_file = os.path.join(path_to_tmp, file)
+            if os.path.exists(add_file):
+                zf.write(add_file, os.path.basename(add_file))
+            print(add_file)
+
+
+def test_read_csv_file():
+    with zipfile.ZipFile(path_to_archive, 'r') as zf:
+        with zf.open('csv_file.csv') as csv_file:
+            content = csv_file.read().decode('utf-8-sig')
+            print(content)
+
+
+def test_read_pdf_file():
+    with zipfile.ZipFile(path_to_archive, 'r') as zf:
+        with zf.open('pdf_file.pdf') as pdf_file:
+            reader = PdfReader(pdf_file)
+            print(reader.pages[0].extract_text())
+
+
+def test_read_xlsx_file():
+    with zipfile.ZipFile(path_to_archive, 'r') as zf:
+        with zf.open('xlsx_file.xlsx') as xlsx:
+            xlsx_file = pd.read_excel(xlsx)
+            print(xlsx_file)

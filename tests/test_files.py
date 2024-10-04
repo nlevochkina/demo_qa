e386e0fd7c4a7ec0e1c6e96dd98465bb48572101
nlevochkina <nlevochkina@ozon.ru>
2024-10-04 14:44:01 +0300

    Работа с файлами

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,8 +1,7 @@
 import os
 import zipfile
-import unittest
-import openpyxl
 import pandas as pd
+import pytest
 
 from pypdf import PdfReader
 
@@ -11,34 +10,34 @@ path_to_tmp = os.path.abspath(os.path.join(path, '..', 'tmp'))
 path_to_archive = os.path.abspath(os.path.join(path_to_tmp, 'files_zip.zip'))
 
 
-class TestsFiles(unittest.TestCase):
+@pytest.fixture(scope='function', autouse=False)
+def test_add_files_to_zip():
+    with zipfile.ZipFile(path_to_archive, 'w') as zf:
+        files_to_add = ['pdf_file.pdf', 'xlsx_file.xlsx', 'csv_file.csv']
+        for file in files_to_add:
+            add_file = os.path.join(path_to_tmp, file)
+            if os.path.exists(add_file):
+                zf.write(add_file, os.path.basename(add_file))
+            print(add_file)
+    yield
 
-    def test_add_files_to_zip(self):
-        with zipfile.ZipFile(path_to_archive, 'w') as zf:
-            files_to_add = ['pdf_file.pdf', 'xlsx_file.xlsx', 'csv_file.csv']
-            for file in files_to_add:
-                add_file = os.path.join(path_to_tmp, file)
-                if os.path.exists(add_file):
-                    zf.write(add_file, os.path.basename(add_file))
-                print(add_file)
 
-    def test_read_csv_file(self):
-        with zipfile.ZipFile(path_to_archive, 'r') as zf:
-            with zf.open('csv_file.csv') as csv_file:
-                content = csv_file.read().decode('utf-8-sig')
-                print(content)
+def test_read_csv_file(test_add_files_to_zip):
+    with zipfile.ZipFile(path_to_archive, 'r') as zf:
+        with zf.open('csv_file.csv') as csv_file:
+            content = csv_file.read().decode('utf-8-sig')
+            print(content)
 
-    def test_read_pdf_file(self):
-        with zipfile.ZipFile(path_to_archive, 'r') as zf:
-            with zf.open('pdf_file.pdf') as pdf_file:
-                reader = PdfReader(pdf_file)
-                print(reader.pages[0].extract_text())
 
-    def test_read_xlsx_file(self):
-        with zipfile.ZipFile(path_to_archive, 'r') as zf:
-            with zf.open('xlsx_file.xlsx') as xlsx:
-                xlsx_file = pd.read_excel(xlsx)
-                print(xlsx_file)
+def test_read_pdf_file(test_add_files_to_zip):
+    with zipfile.ZipFile(path_to_archive, 'r') as zf:
+        with zf.open('pdf_file.pdf') as pdf_file:
+            reader = PdfReader(pdf_file)
+            print(reader.pages[0].extract_text())
 
-    if test_add_files_to_zip == '__main__':
-        unittest.main()
+
+def test_read_xlsx_file(test_add_files_to_zip):
+    with zipfile.ZipFile(path_to_archive, 'r') as zf:
+        with zf.open('xlsx_file.xlsx') as xlsx:
+            xlsx_file = pd.read_excel(xlsx)
+            print(xlsx_file)
